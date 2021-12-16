88fc2522e523a923d437aa2999d16f92f3f3155d
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-12-16 15:42:37 +0100

    Implemented suggested changes in convert.

--- tests/core/data/test_convert.py
+++ tests/core/data/test_convert.py
@@ -5,21 +5,24 @@ import pytest
 import qutip
 from qutip_tensornetwork.core.data import Network
 from qutip import data
-from .conftest import random_complex_network, random_numpy_dense
-
-_network = random_complex_network(2)
+from .conftest import random_one_node_network, random_numpy_dense
 
 @pytest.mark.parametrize("copy", [True, False])
-def test_create(copy):
-    qobj = qutip.Qobj(_network, copy=copy)
+@pytest.mark.parametrize("shape",
+                         [(2,2), (2,1), (1,2), (1,1)],
+                         ids=["operator", "ket", "bra", "scalar"])
+def test_create(shape, copy):
+    network = random_one_node_network(shape)
+    qobj = qutip.Qobj(network, copy=copy)
     assert isinstance(qobj.data, Network)
+    assert qobj.shape == shape
     if copy:
-        assert qobj.data is not _network
-        np.testing.assert_allclose(qobj.full(), _network.to_array())
+        assert qobj.data is not network
+        np.testing.assert_allclose(qobj.full(), network.to_array())
         #TODO: This does not test that both networks are copies directly but
         # testing that is not straightforward
     else:
-        assert qobj.data is _network
+        assert qobj.data is network
 
 
 # We only test for the dense to network because we expect the rest to work if 
@@ -29,16 +32,22 @@ def test_create(copy):
                          ids=["operator", "ket", "bra", "scalar"])
 @pytest.mark.parametrize("dtype",
                          ["network", Network],
-                         ids=["string `network`", "data type"])
+                         ids=["string `network`",  "data type"])
 def test_convert_dense_to_network(shape, dtype):
     array = random_numpy_dense(shape)
     qobj = qutip.Qobj(array)
     qobj = qobj.to(dtype)
     assert isinstance(qobj.data, Network)
+    assert qobj.shape == shape
     np.testing.assert_allclose(qobj.full(), array)
 
-def test_network_to_dense():
-    qobj = qutip.Qobj(_network)
+@pytest.mark.parametrize("shape",
+                         [(2,2), (2,1), (1,2), (1,1)],
+                         ids=["operator", "ket", "bra", "scalar"])
+def test_network_to_dense(shape):
+    network = random_one_node_network(shape)
+    qobj = qutip.Qobj(network)
     qobj = qobj.to("dense")
     assert isinstance(qobj.data, qutip.data.Dense)
-    np.testing.assert_allclose(qobj.full(), _network.to_array())
+    assert qobj.shape == shape
+    np.testing.assert_allclose(qobj.full(), network.to_array())
