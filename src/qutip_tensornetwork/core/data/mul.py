dfc4da0ff95bc52c5473f8d9ce0370565a003fea
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-12-28 17:46:10 +0100

    imul and neg where not being added to the dispatcher.

--- src/qutip_tensornetwork/core/data/mul.py
+++ src/qutip_tensornetwork/core/data/mul.py
@@ -27,11 +27,10 @@ def neg_network(network):
     """
     Perform the operation
         ``-1 * network``
-    where ``network`` is an instance of ``Network`` and value is a ``scalar``
-    value.
+    where ``network`` is an instance of ``Network``.
     """
     return network * -1
 
 qutip.data.mul.add_specialisations([(Network, Network, mul_network)])
-qutip.data.mul.add_specialisations([(Network, Network, imul_network)])
-qutip.data.mul.add_specialisations([(Network, Network, neg_network)])
+qutip.data.imul.add_specialisations([(Network, Network, imul_network)])
+qutip.data.neg.add_specialisations([(Network, Network, neg_network)])
